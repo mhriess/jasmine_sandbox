098c61c563f20b4ea5a89eb35d19b07d5b507b80
Mike Riess <mikeriess@gmail.com>
2012-11-19 11:55:03 -0800

    Add specs for find missing number

--- spec/Spec.js
+++ spec/Spec.js
@@ -1,15 +1,34 @@
 // Your Jasmine specs here
 describe("factorial", function() {
-  it("evaluates correctly for a single-digit number", function() {
-    expect(factorial(5)).toEqual(120);
-  });
+	it("evaluates correctly for a single-digit number", function() {
+		expect(factorial(5)).toEqual(120);
+	});
 
-  it("evaluates correctly for a double-digit number", function() {
-	expect(factorial(10)).toEqual(3628800)
-  });
+	it("evaluates correctly for a double-digit number", function() {
+		expect(factorial(10)).toEqual(3628800)
+	});
 
-  it("evaluates correctyl when given 0 as an input", function() {
-	expect(factorial(0)).toEqual(1)
-  });
+	it("evaluates correctyl when given 0 as an input", function() {
+		expect(factorial(0)).toEqual(1)
+	});
 });
 
+describe("missing number loop", function() {
+	it("guesses the missing number", function() {
+		expect(find_missing_loop([1,2,3,4,6])).toEqual(5)
+	});
+	
+	it("returns a message when no number is missing", function() {
+		expect(find_missing_loop([1,2,3,4,5])).toEqual("All there")
+	})
+});
+
+describe("missing number math", function() {
+	it("guesses the missing number", function() {
+		expect(find_missing_math([1,2,3,4,6])).toEqual(5)
+	});
+	
+	it("returns a message when no number is missing", function() {
+		expect(find_missing_math([1,2,3,4,5])).toEqual("All there")
+	})
+});
--- src/Script.js
+++ src/Script.js
@@ -6,3 +6,32 @@ var factorial = function(n) {
   }
   return result;
 };
+
+var find_missing_loop = function(a) {
+  var sorted = a.sort();
+  for (var i=1; i<=sorted.length; i++) {
+    if (i !== sorted[i-1]) {
+      return i;
+    };
+  };
+  return "All there";
+}
+
+// Math
+var factorial_sum = function(num) {
+  for (var i=(num-1); i > 0; i--) {
+    num = num + i;
+  };
+  return num;
+};
+
+var find_missing_math = function(a) {
+  var supposed_sum = factorial_sum(a.length + 1);
+  var actual_sum = eval(a.join("+"));
+  var net = supposed_sum - actual_sum;
+  if (net > a[a.length - 1]) {
+    return "All there";
+  } else {
+    return net;
+  };
+}
